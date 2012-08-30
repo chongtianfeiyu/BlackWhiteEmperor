0b9f55db3b5598767642c1223068db951627db51
Rothcold <rothcold@me.com>
2012-08-30 22:02:19 +0800

    重构了发牌算法

---
+++ test/controller_tests.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("..")
+
+from bwe import *
+import unittest
+import random
+class TestModelFunctions(unittest.TestCase):
+    def setUp(self):
+        return
+
+if __name__ == '__main__':
+    unittest.main()
